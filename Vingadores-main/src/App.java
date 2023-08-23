2cc490fd700517a13fa26f984e0f28c1478efa0f
Cayo Henrique <cayoh000@gmail.com>
2023-08-23 19:41:08 -0300

    Personagem

--- Vingadores-main/src/App.java
+++ Vingadores-main/src/App.java
@@ -9,10 +9,17 @@ public class App {
         System.out.println("Você é convocado para ajudar os Vingadores em uma missão.");
         System.out.println("Você deseja se juntar ao Capitão América ou ao Homem de Ferro? (Capitão/Homem de Ferro)");
         String escolhaPrimaria = leitor.nextLine();
+        
+        Personagem heroi1 = new Personagem("Capitao", 100);
+        
+        Personagem heroi2 = new Personagem("Homem de Ferro", 70);
+      
+        if (escolhaPrimaria.equalsIgnoreCase("Capitao")) {
 
-        if (escolhaPrimaria.equalsIgnoreCase("Capitão")) {
+            
             // Desenvolvimento da escolha 1
             System.out.println("Você se junta ao Capitão América e vai para uma batalha.");
+            System.out.println("Energia do " + heroi1.nome + ": " + heroi1.pv);
             System.out.println("Você vê um helicóptero prestes a cair. O que você faz? (Salvar/Lutar)");
             String escolha2 = leitor.nextLine();
 
@@ -20,16 +27,22 @@ public class App {
                 // Desenvolvimento da escolha 2 (a)
                 System.out.println("Você salva o helicóptero e é elogiado pela equipe.");
                 System.out.println("Parabéns! Você alcançou o final heroico.");
+                heroi1.mostrarStatus(-60);
+                
             } else if (escolha2.equalsIgnoreCase("Lutar")) {
                 // Desenvolvimento da escolha 2 (b)
                 System.out.println("Você decide lutar contra o inimigo, mas é ferido gravemente.");
                 System.out.println("Infelizmente, você não consegue se recuperar. Fim do livro.");
+                heroi1.mostrarStatus(-100);
             } else {
                 System.out.println("Opção inválida. Fim do livro.");
             }
         } else if (escolhaPrimaria.equalsIgnoreCase("Homem de Ferro")) {
+            
+            
             // Desenvolvimento da escolha 1
             System.out.println("Você se junta ao Homem de Ferro e embarca em uma missão de resgate.");
+            System.out.println("Energia do "+ heroi2.nome + ": " + heroi2.pv);
             System.out.println("Você encontra um dispositivo alienígena. O que você faz? (Analisar/Ativar)");
             String escolha2 = leitor.nextLine();
 
@@ -38,10 +51,14 @@ public class App {
                 System.out.println("Você analisa o dispositivo e descobre sua função secreta.");
                 System.out.println("Com essa informação, você ajuda a derrotar o vilão e salva o dia!");
                 System.out.println("Parabéns! Você alcançou o final heróico.");
+                heroi2.mostrarStatus(-20);
+
+                
             } else if (escolha2.equalsIgnoreCase("Ativar")) {
                 // Desenvolvimento da escolha 2 (d)
                 System.out.println("Ao ativar o dispositivo, você acidentalmente liberta uma força maligna.");
                 System.out.println("Infelizmente, o mundo fica em perigo e não há como consertar o erro. Fim do livro.");
+                heroi2.mostrarStatus(-70);
             } else {
                 System.out.println("Opção inválida. Fim do livro.");
             }
--- Vingadores-main/src/Personagem.java
+++ Vingadores-main/src/Personagem.java
@@ -1,5 +1,36 @@
-package io.github.jiangdequan;
-
 public class Personagem {
+
+    String nome;
+    int pv;
+
+    Personagem(String nome, int pv){
+        this.nome = nome;
+        this.pv = pv;
+    }
+
+    void mostrarStatus(int pv) {
+        if(pv <= 0) {
+            this.pv += pv;
+            
+            System.out.println("O "+ this.nome + " perdeu energia.");
+            System.out.println("Energia: "+ this.pv);
+          
+            
+        
+        } else if(pv > 0 ) { 
+            this.pv += pv;
+             
+             System.out.println("O "+ this.nome + " ganhou energia.");
+             System.out.println("Energia: "+ this.pv);
+        }
+        else {
+             System.out.println("A Energia continua a mesma");
+             System.out.println("Energia: "+ this.pv);
+        }
+
+    }
+        
+
+        
+    }
     
-}
